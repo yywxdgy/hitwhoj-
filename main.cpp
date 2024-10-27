7289fa2da9769066031eaa1c629cc87ccd9fdb5c
yywxdgy <2421594879@qq.com>
2024-10-27 15:42:49 +0800

    Remove puppeteer and use axios instead.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,132 @@
-#include <stdio.h>
-#define N 1010
-int n, m, opt;
-int pr[N], tr[N];
-int G[N][N];
-void Inc(int x, int y, int z) { G[x][y] = z; }
+#include <bits/stdc++.h>
+#define N 5010
+#define int long long
+using namespace std;
+int n, m;
+int c[N][N] = {}, cc[N][N] = {};
+int c1[N][N] = {}, c2[N][N] = {};
 int read() {
-    int s = 0; char c = getchar(); 
-    for (; c < '0' || c > '9'; c = getchar());
-    for (s = 0; c >= '0' && c <= '9'; c = getchar())
-        s = (s << 3) + (s << 1) + c - 48;
-    return s;
-} 
-char readc() {
+    int s = 0, w = 1;
     char c = getchar();
-    for (; (c < 'A' || c > 'Z'); c = getchar());
-    return c;
-} 
-int vis[N], vs = 0;
-void Dfs(int u) {
-    int i, v;
-    vis[u] = 1, ++vs;
-    putchar(pr[u]);
-    for (i = 1; i <= n; ++i) {
-        v = i;
-        if (!G[u][v]) continue;
-        if (vis[v]) continue;
-        Dfs(v);
-    } 
+
+    while ((c > '9' || c < '0') && c != '-')
+        c = getchar();
+
+    if (c == '-')
+        w = -1, c = getchar();
+
+    while (c <= '9' && c >= '0')
+        s = (s << 3) + (s << 1) + c - '0', c = getchar();
+
+    return s * w;
 }
-int main() {
-    int i, x, y, z = 1;
-    opt = read(), n = read(), m = read();
-    for (i = 1; i <= n; ++i) pr[i] = readc(), tr[pr[i]] = i;
-    for (i = 1; i <= m; ++i) 
-        x = readc(), y = readc(), 
-        ((opt == 1 || opt == 3) ? z = read() : 0), 
-        Inc(tr[x], tr[y], z), 
-        (opt == 2 || opt == 3) ? Inc(tr[y], tr[x], z),0 : 0;
-    int st = tr[(int)readc()];
-    Dfs(st);
-    if (vs == n) return 0; 
-    for (i = 1; i <= n; ++i) if (!vis[i]) Dfs(i);
+void inc(int x, int y, int v) {
+    int _ = y;
+
+    for (; x <= N; x += x & (-x))
+        for (y = _; y <= N; y += y & (-y))
+            c[x][y] += v;
+}
+int ask(int x, int y) {
+    int sum = 0, _ = y;
+
+    for (; x; x -= x & (-x))
+        for (y = _; y; y -= y & (-y))
+            sum += c[x][y];
+
+    return sum;
+}
+void incc(int x, int y, int v) {
+    int _ = y, __ = x - 1, ___ = y - 1;
+
+    for (; x <= N; x += x & (-x))
+        for (y = _; y <= N; y += y & (-y))
+            cc[x][y] += __ * ___ * v;
+}
+int askk(int x, int y) {
+    int sum = 0, _ = y;
+
+    for (; x; x -= x & (-x))
+        for (y = _; y; y -= y & (-y))
+            sum += cc[x][y];
+
+    return sum;
+}
+void innc(int x, int y, int v) {
+    int _ = y, __ = x - 1;
+
+    for (; x <= N; x += x & (-x))
+        for (y = _; y <= N; y += y & (-y))
+            c1[x][y] += __ * v;
+}
+int assk(int x, int y) {
+    int _ = y, sum = 0;
+
+    for (; x; x -= x & (-x))
+        for (y = _; y; y -= y & (-y))
+            sum += c1[x][y];
+
+    return sum;
+}
+void iinc(int x, int y, int v) {
+    int _ = y, ___ = y - 1;
+
+    for (; x <= N; x += x & (-x))
+        for (y = _; y <= N; y += y & (-y))
+            c2[x][y] += ___ * v;
+}
+int aask(int x, int y) {
+    int _ = y, sum = 0;
+
+    for (; x; x -= x & (-x))
+        for (y = _; y; y -= y & (-y))
+            sum += c2[x][y];
+
+    return sum;
+}
+int Ask(int x, int y) {
+    return (x) * (y) * ask(x, y) - (y) * assk(x, y) - (x) * aask(x, y) + askk(x, y);
+}
+void pr() {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j)
+            printf("%lld ", askk(i, j));
+
+        puts("");
+    }
+}
+signed main() {
+    n = read();
+    m = read();
+    int op = 0;
+
+    while (scanf("%lld", &op) != EOF) {
+        if (op == 1) {
+            int l1 = read(), r1 = read(), l2 = read(), r2 = read(), v = read();
+            inc(l1 + 1, r1 + 1, v);
+            inc(l1 + 1, r2 + 2, -v);
+            inc(l2 + 2, r1 + 1, -v);
+            inc(l2 + 2, r2 + 2, v);
+            incc(l1 + 1, r1 + 1, v);
+            incc(l1 + 1, r2 + 2, -v);
+            incc(l2 + 2, r1 + 1, -v);
+            incc(l2 + 2, r2 + 2, v);
+            innc(l1 + 1, r1 + 1, v);
+            innc(l1 + 1, r2 + 2, -v);
+            innc(l2 + 2, r1 + 1, -v);
+            innc(l2 + 2, r2 + 2, v);
+            iinc(l1 + 1, r1 + 1, v);
+            iinc(l1 + 1, r2 + 2, -v);
+            iinc(l2 + 2, r1 + 1, -v);
+            iinc(l2 + 2, r2 + 2, v);
+            //          pr();
+        }
+
+        if (op == 2) {
+            int l1 = read(), r1 = read(), l2 = read(), r2 = read();
+            printf("%lld\n", Ask(l2 + 1, r2 + 1) - Ask(l2 + 1, r1) - Ask(l1, r2 + 1) + Ask(l1, r1));
+        }
+    }
+
     return 0;
-} 
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const args = process.argv.slice(2);
@@ -6,58 +6,28 @@ const args = process.argv.slice(2);
 async function run(id) {
     // console.log("Loading the page...", ' ');
     
-    const url = `http://1.94.128.12/problem/${id}/submit`;
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    // console.log("Done.");
-    // console.log("Reading the file(main.cpp)...");
-    
-
-    let codes;
-    let file = path.resolve(__dirname, './main.cpp');
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) throw err;
-        codes = String(data);
-    });
-
-    // console.log("Done.");
-    // console.log("Reading the file(cookies.json)...");
-
+    const url = `http://localhost:8080/problem/${id}/submit?_data=routes%2Fproblem%2F%24problemId%2Fsubmit`;
     let cookies=JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
-    
-    // console.log("Done.");
-    // console.log("Setting the cookies...");
-    
-    
-    await page.setCookie(...cookies);
-    await page.reload();
-
-    // console.log("Done.");
-    // console.log("Submitting the code...");
-
-    const text = codes;
-    const block = 'body > div > div.drawer-content.flex.h-full.flex-col > div.flex-1.p-6 > div > form > div:nth-child(2) > textarea';
-    const submit = 'body > div > div.drawer-content.flex.h-full.flex-col > div.flex-1.p-6 > div > form > div:nth-child(3) > button';
-    const language = 'body > div > div.drawer-content.flex.h-full.flex-col > div.flex-1.p-6 > div > form > div:nth-child(1) > select';
-
-    await page.waitForSelector(block);
-    await page.waitForSelector(language);
-    await page.select(language, 'cpp');
-    await page.click(block);
-    
-    const lines = text.split('\n');
-    for (const line of lines) {
-        const te = String(line);
-        await page.keyboard.type(te);
-    }
-    await page.waitForSelector(submit);
-    await page.click(submit);
-
-    // console.log("Submitted.");
-    
-    await browser.close();
+    cookies=cookies.map(cookie=>cookie.name+'='+cookie.value).join('; ');
+    let codes=String(fs.readFileSync("main.cpp", 'utf8'));
+    const config = {
+        method : 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': cookies,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        },
+        data: {
+            "_data": "routes/problem/$problemId/submit",
+            "language": "cpp",
+            "code": codes
+        }
+    };
+    await axios(url, config).then((res) => {
+        console.log(res.data);
+    }).catch((err) => {
+        console.log(err);
+    });
 }
 
 run(args[0]);
